b988b51968acb286a9e84374aef1570e8491cad7
Nopfed <robby.tsunami@gmail.com>
2025-11-20 18:40:42 -0500

    Repo init

---
+++ picker.gd
@@ -0,0 +1,67 @@
+extends Control
+
+const CLASSES = {
+	'warrior': {
+		'races' : [
+			'human',
+			'dwarf',
+			'orc',
+			'troll',
+			'gnome',
+			'nightelf',
+			'undead',
+			'tauren'
+		],
+		'specs': ['protection', 'arms', 'fury']
+	},
+	'paladin': {
+		'races' : ['human', 'dwarf'],
+		'specs': ['protection', 'holy', 'retribution']
+	},
+	'rogue': {
+		'races' : ['human', 'dwarf', 'orc', 'troll', 'gnome', 'nightelf', 'undead'],
+		'specs': ['assassination', 'combat', 'subtlety']
+	},
+	'priest': {
+		'races' : ['human', 'dwarf', 'troll', 'nightelf', 'undead'],
+		'specs': ['Discipline', 'Holy', 'Shadow']
+	},
+	'mage': {
+		'races' : ['human', 'troll', 'gnome', 'undead'],
+		'specs': ['Arcane', 'Fire', 'Frost']
+	},
+	'warlock': {
+		'races' : ['human', 'orc', 'gnome', 'undead'],
+		'specs': ['Demonology', 'Affliction', 'Destruction']
+	},
+	'hunter': {
+		'races' : ['dwarf', 'orc', 'troll', 'nightelf', 'tauren'],
+		'specs': ['Beast Master', 'Survival', 'Marksmanship']
+	},
+	'druid': {
+		'races' : ['nightelf', 'tauren'],
+		'specs': ['Restoration', 'Balance', 'Feral Combat']
+	},
+	'shaman': {
+		'races' : ['orc', 'troll', 'tauren'],
+		'specs': ['Restoration', 'Elemental', 'Enhancement']
+	}
+}
+
+func getRandomDictItem(dict: Dictionary, returnKey = false):
+	var keys = dict.keys() # Get all keys as an array
+	var random_index = randi() % dict.size() # Generate a random index
+	
+	if returnKey:
+		return [dict[keys[random_index]], keys[random_index]]
+	
+	return dict[keys[random_index]] # Return the value associated with the random key
+
+
+func _on_button_button_down() -> void:
+	var gender = ['male', 'female'].pick_random()
+	var randomClass: String = getRandomDictItem(CLASSES, true)[1]
+	var race: String = CLASSES[randomClass]['races'].pick_random()
+	var spec: String = CLASSES[randomClass]['specs'].pick_random()
+	
+	$Label.text = gender + ' ' + race + ' ' + spec + ' ' + randomClass
